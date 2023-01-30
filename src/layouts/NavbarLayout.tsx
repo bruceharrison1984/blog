c8b07736f9322d5aea346a6615c3c0ba6f119d96
Bruce Harrison <leeharrison1984@gmail.com>
2023-01-30 16:48:47 -0600

    try to make style better

--- src/layouts/NavbarLayout.tsx
+++ src/layouts/NavbarLayout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from '@/components/navbar/Navbar';
+import { Navbar2 } from '@/components/navbar2/Navbar2';
 import { ReactNode } from 'react';
 import { useRouter } from 'next/router';
 
@@ -6,7 +7,8 @@ export const NavbarLayout = ({ children }: { children: ReactNode }) => {
   const { pathname } = useRouter();
   return (
     <div className="flex flex-col h-screen">
-      <Navbar currentRoute={pathname} />
+      {/* <Navbar currentRoute={pathname} /> */}
+      <Navbar2 />
       <main className="p-6 justify-center h-screen overflow-y-auto">
         {children}
       </main>
