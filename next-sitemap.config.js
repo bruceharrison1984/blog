40945f9643eee0c7a4568bbae20f7a62e7b4ff01
Bruce Harrison <leeharrison1984@gmail.com>
2023-02-01 16:10:13 -0600

    add sitemap functionality

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://blog.bruceleeharrison.com',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
