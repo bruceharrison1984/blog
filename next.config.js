f91b4504813b4963c9f7ee1318720ccb0bc2b609
Bruce Harrison <leeharrison1984@gmail.com>
2023-02-02 09:58:32 -0600

    add redirects from old blog locations to new ones

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:year(\\d{4})/:month(\\d{2})/:day(\\d{2})/:slug',
+        destination: '/posts/:year/:slug',
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
