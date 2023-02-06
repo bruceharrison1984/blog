39963ea66f1dde0f2e162de92e8e67b76f804c9c
Bruce Harrison <leeharrison1984@gmail.com>
2023-02-06 16:53:23 -0600

    simplfy nav layout

--- src/layouts/NavbarLayout.tsx
+++ src/layouts/NavbarLayout.tsx
@@ -5,11 +5,11 @@ import { useRouter } from 'next/router';
 export const NavbarLayout = ({ children }: { children: ReactNode }) => {
   const { pathname } = useRouter();
   return (
-    <div className="flex flex-col h-screen">
+    <>
       <Navbar currentPath={pathname} />
-      <main className="p-6 justify-center h-screen overflow-x-hidden">
-        <div className="mt-12">{children}</div>
+      <main className="justify-center h-screen overflow-x-hidden pt-20">
+        {children}
       </main>
-    </div>
+    </>
   );
 };
