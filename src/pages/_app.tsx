d8917922c4e87346435dfbbb321bdc88797b13bc
Bruce Harrison <leeharrison1984@gmail.com>
2023-02-07 09:48:25 -0600

    add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import '../styles/globals.css';
-import { Inter } from '@next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-import { AppProps } from 'next/app';
+import { AppProps, NextWebVitalsMetric } from 'next/app';
 import { BackgroundEffect } from '@/components/backgroundEffect/BackgroundEffect';
+import { GoogleAnalytics, event } from 'nextjs-google-analytics';
+import { Inter } from '@next/font/google';
 import { NavbarLayout } from '@/layouts/NavbarLayout';
 import Head from 'next/head';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -18,6 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         <meta name="theme-color" content="#3D4451"></meta>
       </Head>
+      <GoogleAnalytics trackPageViews />
       <BackgroundEffect />
       <div className={inter.className}>
         <NavbarLayout>
@@ -26,4 +28,18 @@ export default function App({ Component, pageProps }: AppProps) {
       </div>
     </>
   );
+}
+
+export function reportWebVitals({
+  id,
+  name,
+  label,
+  value,
+}: NextWebVitalsMetric) {
+  event(name, {
+    category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
 }
